cb82ede9c4536b63f247e3f769de27fdd3ba184c
Jared Davidson (Archetapp) <archetapp@gmail.com>
2020-11-02 13:07:23 -0700

    Initial Commit

---
+++ CodingChallenge/CodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodingChallengeApp.swift
+//  CodingChallenge
+//
+//  Created by Jared on 11/2/20.
+//
+
+import SwiftUI
+
+@main
+struct CodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
